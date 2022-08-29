52c98a1fab355cad646a145f5f0dd39bc915db3f
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2022-08-29 14:47:26 +0300

    add 8 homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const elMarkup = createPhotoCardsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', elMarkup);
+
+function createPhotoCardsMarkup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}"/>
+                </a>`;
+    })
+    .join('');
+}
+
+galleryEl.addEventListener('click', noCroosToAnotherPage);
+
+function noCroosToAnotherPage(evt) {
+  evt.preventDefault();
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+// const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
+let formValue = {};
+
+const formElement = document.querySelector('form');
+//   textarea: document.querySelector('textarea[name="message"]'),
+//   input: document.querySelector('input[name="email"]'),
+const savedMessage = localStorage.getItem('feedback-form-state');
+
+formElement.addEventListener('submit', onFormSubmit);
+formElement.addEventListener('input', throttle(onTextValueInput, 500));
+
+// valueTextAreaInput();
+
+function onFormSubmit(evt) {
+  git;
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  formValue = {};
+  console.log(formValue);
+}
+
+if (savedMessage) {
+  formValue = JSON.parse(savedMessage);
+
+  for (let key in formValue) {
+    formElement.elements[key].value = formValue[key];
+  }
+}
+
+// function onTextInputValue(evt) {
+//   const message = evt.target.value;
+//   localStorage.setItem('STORAGE_KEY', message);
+//   //   console.log(message);
+// }
+function onTextValueInput(evt) {
+  formValue[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+}
+
+// function valueTextAreaInput() {
+//   const savedMessage = localStorage.getItem('STORAGE_KEY');
+//   if (savedMessage) {
+//     console.log(savedMessage);
+//     refs.textarea.value = savedMessage;
+//   }
+// }
